7618c3f29dcd48589d58559e9f01d068227f4239
Maxime Charland <charland.maxime@gmail.com>
2023-10-02 22:28:15 -0400

    Create Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'rsvp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatExpansionPanel)
+  panel?: MatExpansionPanel;
+
+  private scrollCallback = () => this.changeDetector.detectChanges();
+
+  constructor(protected changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scrollCallback);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.scrollCallback);
+  }
+
+  get scrollY() {
+    console.log(window.scrollY);
+    return window.scrollY;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { DefaultHomeComponent } from './default-home/default-home.component';
@@ -16,6 +22,12 @@ import { DefaultHomeComponent } from './default-home/default-home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
